948890a38d469eabcb15b85c7ec2374d940d99c6
Lukas Rambold <lukas@rambold.io>
2019-11-19 16:48:27 +0100

    implemented GetTable

--- src/lib/operators/get_table.hpp
+++ src/lib/operators/get_table.hpp
@@ -3,6 +3,7 @@
 #include <memory>
 #include <string>
 #include <vector>
+#include <storage/storage_manager.hpp>
 
 #include "abstract_operator.hpp"
 
@@ -11,11 +12,22 @@ namespace opossum {
 // operator to retrieve a table from the StorageManager by specifying its name
 class GetTable : public AbstractOperator {
  public:
-  explicit GetTable(const std::string& name);
+  explicit GetTable(const std::string& name) {
+      auto& sm = StorageManager::get();
+      _table = sm.get_table(name);
+      _name = name;
+  }
 
-  const std::string& table_name() const;
+    const std::string table_name() const {
+        return _name;
+    }
 
- protected:
-  std::shared_ptr<const Table> _on_execute() override;
+protected:
+  std::shared_ptr<const Table> _on_execute() override {
+      return _table;
+  }
+  // TODO checkout whether we could use const here
+  std::string _name;
+  std::shared_ptr<const Table> _table;
 };
 }  // namespace opossum
