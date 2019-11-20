0eeb21623ddde0b82b8340b334e6d9c8d0a0febc
Jan Kossmann <jan.kossmann@hpi.de>
2019-11-20 15:00:12 +0100

    Fix test inheritance

--- src/test/storage/reference_segment_test.cpp
+++ src/test/storage/reference_segment_test.cpp
@@ -6,6 +6,7 @@
 #include <utility>
 #include <vector>
 
+#include "../base_test.hpp"
 #include "gtest/gtest.h"
 
 #include "operators/abstract_operator.hpp"
@@ -19,7 +20,7 @@
 
 namespace opossum {
 
-// class ReferenceSegmentTest : public ::testing::Test {
+// class ReferenceSegmentTest : public BaseTest {
 //   virtual void SetUp() {
 //     _test_table = std::make_shared<opossum::Table>(opossum::Table(3));
 //     _test_table->add_column("a", "int");
