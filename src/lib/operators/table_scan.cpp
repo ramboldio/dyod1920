e93b487bc7aa03ea9b6d236b243dce94b523709d
Lukas Böhme <lukas.boehme@icloud.com>
2019-11-23 15:49:58 +0100

    Impl protected values and getter for TableScan

--- src/lib/operators/table_scan.cpp
+++ src/lib/operators/table_scan.cpp
@@ -1,4 +1,36 @@
-//
-// Created by Lukas Böhme on 22.11.19.
-//
+#include "table_scan.hpp"
+
+namespace opossum {
+    //Constructor
+    TableScan::TableScan(const std::shared_ptr<const AbstractOperator> in, ColumnID column_id, const ScanType scan_type,
+                         const AllTypeVariant search_value) : _column_id(column_id), _scan_type(scan_type), _search_value(search_value) {
+
+    }
+
+    //Deconstructor
+    TableScan::~TableScan() {
+
+    }
+
+    //Protected functions
+    std::shared_ptr<const Table> TableScan::_on_execute() {
+
+
+
+        return std::shared_ptr<const Table>();
+    }
+
+    //Getter
+    ColumnID TableScan::column_id() const {
+        return _column_id;
+    }
+
+    ScanType TableScan::scan_type() const {
+        return _scan_type;
+    }
+
+    const AllTypeVariant &TableScan::search_value() const {
+        return _search_value;
+    }
+}
 
