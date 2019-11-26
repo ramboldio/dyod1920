c9e0391fd4c9b8b7958b70d675800e149d15888e
Lukas Böhme <lukas.boehme@icloud.com>
2019-11-26 20:20:44 +0100

    Added test for valuesegment scan

--- src/test/storage/value_segment_test.cpp
+++ src/test/storage/value_segment_test.cpp
@@ -14,6 +14,8 @@ class StorageValueSegmentTest : public BaseTest {
   ValueSegment<int> int_value_segment;
   ValueSegment<std::string> string_value_segment;
   ValueSegment<double> double_value_segment;
+
+  ValueSegment<int> scan_value_segment;
 };
 
 TEST_F(StorageValueSegmentTest, GetSize) {
@@ -52,6 +54,40 @@ TEST_F(StorageValueSegmentTest, MemoryUsage) {
   EXPECT_EQ(int_value_segment.estimate_memory_usage(), size_t{4});
   int_value_segment.append(2);
   EXPECT_EQ(int_value_segment.estimate_memory_usage(), size_t{8});
+}
+
+TEST_F(StorageValueSegmentTest, ScanValueSegmentOrdered) {
+
+    // values from 0 - 99
+    for (int i = 0; i <= 99; i++){
+        scan_value_segment.append(i);
+    }
+
+    auto pos_list = std::make_shared<PosList>(PosList());
+
+    scan_value_segment.scan(ScanType::OpEquals, 95, ChunkID(0), pos_list);
+    EXPECT_EQ(pos_list->size(), 1);
+    pos_list->clear();
+
+    scan_value_segment.scan(ScanType::OpGreaterThan, 95, ChunkID(0), pos_list);
+    EXPECT_EQ(pos_list->size(), 4);
+    pos_list->clear();
+
+    scan_value_segment.scan(ScanType::OpGreaterThanEquals, 90, ChunkID(0), pos_list);
+    EXPECT_EQ(pos_list->size(), 10);
+    pos_list->clear();
+
+    scan_value_segment.scan(ScanType::OpLessThan, 30, ChunkID(0), pos_list);
+    EXPECT_EQ(pos_list->size(), 30);
+    pos_list->clear();
+
+    scan_value_segment.scan(ScanType::OpLessThanEquals, 10, ChunkID(0), pos_list);
+    EXPECT_EQ(pos_list->size(), 11);
+    pos_list->clear();
+
+    scan_value_segment.scan(ScanType::OpNotEquals, 80, ChunkID(0), pos_list);
+    EXPECT_EQ(pos_list->size(), 99);
+    pos_list->clear();
 }
 
 }  // namespace opossum
